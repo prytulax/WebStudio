3bf24536349cd0b582f8c5345ec04e025162692a
Nazar <164397254+prytulax@users.noreply.github.com>
2024-07-28 22:53:54 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+
+        const data = ['apple', 'banana', 'cherry', 'date', 'elderberry', 'fig', 'grape'];
+
+        const searchInput = document.getElementById('searchInput');
+        const resultsList = document.getElementById('results');
+
+      
